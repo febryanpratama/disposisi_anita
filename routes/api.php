bec0441c045e1a111dea119322a4039f4d5aa79c
febryanpratama <spandupan46@student.untan.ac.id>
2023-07-07 20:44:43 +0700

    feat: rev all data

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('send-sms', [FrontController::class, 'sendsms']);
+Route::post('send-sms', [FrontController::class, 'sendsms']);
