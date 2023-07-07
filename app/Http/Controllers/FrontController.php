4aa1c986ff3f8e7b3fce662394e9c39cd5df3ca9
febryanpratama <spandupan46@student.untan.ac.id>
2023-07-07 20:35:52 +0700

    feat: rev front

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class FrontController extends Controller
 {
@@ -18,25 +19,34 @@ class FrontController extends Controller
         $passkey = '695fdb3a186c34dca5d8255a';
         $telepon = $request['no_telpon'];
         $message = $request['messages'];
-        $url = 'https://console.zenziva.net/reguler/api/sendsms/';
-        $curlHandle = curl_init();
-        curl_setopt($curlHandle, CURLOPT_URL, $url);
-        curl_setopt($curlHandle, CURLOPT_HEADER, 0);
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
-        curl_setopt($curlHandle, CURLOPT_POST, 1);
-        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, array(
+        // $url = 'https://console.zenziva.net/reguler/api/sendsms/';
+        // $curlHandle = curl_init();
+        // curl_setopt($curlHandle, CURLOPT_URL, $url);
+        // curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+        // curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
+        // curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
+        // curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
+        // curl_setopt($curlHandle, CURLOPT_POST, 1);
+        // curl_setopt($curlHandle, CURLOPT_POSTFIELDS, array(
+        //     'userkey' => $userkey,
+        //     'passkey' => $passkey,
+        //     'to' => $telepon,
+        //     'message' => $message
+        // ));
+        // $results = json_decode(curl_exec($curlHandle), true);
+        // // dd($results);
+        // curl_close($curlHandle);
+
+        // return response()->json($results);
+
+        $response = Http::post('https://console.zenziva.net/reguler/api/sendsms/', [
             'userkey' => $userkey,
             'passkey' => $passkey,
             'to' => $telepon,
             'message' => $message
-        ));
-        $results = json_decode(curl_exec($curlHandle), true);
-        // dd($results);
-        curl_close($curlHandle);
+        ]);
 
-        return response()->json($results);
+        return response()->json($response);
     }
 }
