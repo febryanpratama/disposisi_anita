b614407fd3440b04625133a1718a25da125657de
febryanpratama <spandupan46@student.untan.ac.id>
2023-07-07 20:28:28 +0700

    feat: rev front

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -11,4 +11,32 @@ class FrontController extends Controller
     {
         return view('layouts.base_front');
     }
+
+    public function sendsms($no_telpon = '088744882202', $messages = 'Halo')
+    {
+        $userkey = '59d153f6a599';
+        $passkey = '695fdb3a186c34dca5d8255a';
+        $telepon = $no_telpon;
+        $message = $messages;
+        $url = 'https://console.zenziva.net/reguler/api/sendsms/';
+        $curlHandle = curl_init();
+        curl_setopt($curlHandle, CURLOPT_URL, $url);
+        curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curlHandle, CURLOPT_POST, 1);
+        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, array(
+            'userkey' => $userkey,
+            'passkey' => $passkey,
+            'to' => $telepon,
+            'message' => $message
+        ));
+        $results = json_decode(curl_exec($curlHandle), true);
+        // dd($results);
+        curl_close($curlHandle);
+
+        return response()->json($results);
+    }
 }
