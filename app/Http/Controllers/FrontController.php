c8f5e14243df94ba316678d95a4c53e8e443e20b
febryanpratama <spandupan46@student.untan.ac.id>
2023-09-08 11:07:31 +0700

    feat :rev dashboard

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -14,26 +14,72 @@ class FrontController extends Controller
 {
     //
 
-    public function index()
+    public function index(Request $request)
     {
 
+        // dd($request->all());
         // $proses = Proposal::whereIn('is_status', ['1', '2'])->count();
         // $setuju = Proposal::whereNotIn('is_status', ['1'])->count();
         // $all = Proposal::count();
 
-        $total = Proposal::whereIn('is_status', ['1', '2'])->sum('jumlah_biaya');
-        $user = User::count();
-
-        if (auth()->user()->hasRole('Pemohon')) {
-            $proses = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['2'])->count();
-            $setuju = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['1'])->where('status', 'Selesai')->count();
-            $all = Proposal::where('user_id', Auth::user()->id)->count();
-            $ditolak = Proposal::where('user_id', Auth::user()->id)->where('is_status', '0')->count();
-
-            $proposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->first();
-            $listProposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->limit(5)->get();
-            // dd('admin');
-            // dd($proposal);
+        if($request->periode){
+            $total = Proposal::whereIn('is_status', ['1', '2'])->whereYear('created_at', $request->periode)->sum('jumlah_biaya');
+            $user = User::count();
+    
+            if (auth()->user()->hasRole('Pemohon')) {
+                $proses = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['2'])->count();
+                $setuju = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['1'])->where('status', 'Selesai')->count();
+                $all = Proposal::where('user_id', Auth::user()->id)->count();
+                $ditolak = Proposal::where('user_id', Auth::user()->id)->where('is_status', '0')->count();
+    
+                $proposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->first();
+                $listProposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->limit(5)->get();
+                // dd('admin');
+                // dd($proposal);
+                return view('dashboard', [
+                    'setuju' => $setuju,
+                    'proses' => $proses,
+                    'all' => $all,
+                    'total' => $total,
+                    'pengguna' => $user,
+                    'ditolak' => $ditolak,
+                    'proposal' => $proposal,
+                    'listproposal' => $listProposal
+                ]);
+            }
+    
+            $proses = Proposal::whereIn('is_status', ['2'])->whereYear('created_at', $request->periode)->count();
+            $setuju = Proposal::whereIn('is_status', ['1'])->whereYear('created_at', $request->periode)->where('status', 'Selesai')->count();
+            $all = Proposal::count();
+            $ditolak = Proposal::where('is_status', '0')->whereYear('created_at', $request->periode)->count();
+            $listProposal = Proposal::with('log')->whereYear('created_at', $request->periode)->orderBy('created_at', "DESC")->limit(5)->get();
+    
+    
+            // $arr_chart = [];
+            // for ($i = 1; $i <= 12; $i++) {
+            //     $arr_chart[] = Proposal::whereMonth('created_at', $i)->whereYear('created_at', Carbon::now()->year)->count();
+            // }
+    
+            $januari = Proposal::whereMonth('created_at', '01')->whereYear('created_at', $request->periode)->count();
+            $februari = Proposal::whereMonth('created_at', '02')->whereYear('created_at', $request->periode)->count();
+            $maret = Proposal::whereMonth('created_at', '03')->whereYear('created_at', $request->periode)->count();
+            $april = Proposal::whereMonth('created_at', '04')->whereYear('created_at', $request->periode)->count();
+            $mei = Proposal::whereMonth('created_at', '05')->whereYear('created_at', $request->periode)->count();
+            $juni = Proposal::whereMonth('created_at', '06')->whereYear('created_at', $request->periode)->count();
+            $juli = Proposal::whereMonth('created_at', '07')->whereYear('created_at', $request->periode)->count();
+            $agustus = Proposal::whereMonth('created_at', '08')->whereYear('created_at', $request->periode)->count();
+            $september = Proposal::whereMonth('created_at', '09')->whereYear('created_at', $request->periode)->count();
+            $oktober = Proposal::whereMonth('created_at', '10')->whereYear('created_at', $request->periode)->count();
+            $november = Proposal::whereMonth('created_at', '11')->whereYear('created_at', $request->periode)->count();
+            $desember = Proposal::whereMonth('created_at', '12')->whereYear('created_at', $request->periode)->count();
+    
+            $bansos = Anggaran::where('jenis_anggaran', 'Bantuan Sosial')->where('tahun_anggaran', $request->periode)->sum('nominal_anggaran');
+            $hibah = Anggaran::where('jenis_anggaran', 'Hibah')->where('tahun_anggaran', $request->periode)->sum('nominal_anggaran');
+    
+            $realisasibansos = Proposal::where('jenis_permohonan', 'Bantuan Sosial')->whereYear('created_at', $request->periode)->where('status', 'selesai')->sum('nominal_disetujui_walikota');
+            $realisasihibah = Proposal::where('jenis_permohonan', 'Hibah')->whereYear('created_at', $request->periode)->where('status', 'selesai')->sum('nominal_disetujui_walikota');
+    
+            // dd($arr_chart);
             return view('dashboard', [
                 'setuju' => $setuju,
                 'proses' => $proses,
@@ -41,68 +87,112 @@ class FrontController extends Controller
                 'total' => $total,
                 'pengguna' => $user,
                 'ditolak' => $ditolak,
-                'proposal' => $proposal,
-                'listproposal' => $listProposal
+                'listproposal' => $listProposal,
+                'januari' => $januari,
+                'februari' => $februari,
+                'maret' => $maret,
+                'april' => $april,
+                'mei' => $mei,
+                'juni' => $juni,
+                'juli' => $juli,
+                'agustus' => $agustus,
+                'september' => $september,
+                'oktober' => $oktober,
+                'november' => $november,
+                'desember' => $desember,
+                'bansos' => intval($bansos),
+                'hibah' => intval($hibah),
+                'realisasibansos' => intval($realisasibansos),
+                'realisasihibah' => intval($realisasihibah),
+            ]);
+
+        }else{
+            
+            $total = Proposal::whereIn('is_status', ['1', '2'])->sum('jumlah_biaya');
+            $user = User::count();
+    
+            if (auth()->user()->hasRole('Pemohon')) {
+                $proses = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['2'])->count();
+                $setuju = Proposal::where('user_id', Auth::user()->id)->whereIn('is_status', ['1'])->where('status', 'Selesai')->count();
+                $all = Proposal::where('user_id', Auth::user()->id)->count();
+                $ditolak = Proposal::where('user_id', Auth::user()->id)->where('is_status', '0')->count();
+    
+                $proposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->first();
+                $listProposal = Proposal::with('log')->where('user_id', auth()->user()->id)->orderBy('created_at', "DESC")->limit(5)->get();
+                // dd('admin');
+                // dd($proposal);
+                return view('dashboard', [
+                    'setuju' => $setuju,
+                    'proses' => $proses,
+                    'all' => $all,
+                    'total' => $total,
+                    'pengguna' => $user,
+                    'ditolak' => $ditolak,
+                    'proposal' => $proposal,
+                    'listproposal' => $listProposal
+                ]);
+            }
+    
+            $proses = Proposal::whereIn('is_status', ['2'])->count();
+            $setuju = Proposal::whereIn('is_status', ['1'])->where('status', 'Selesai')->count();
+            $all = Proposal::count();
+            $ditolak = Proposal::where('is_status', '0')->count();
+            $listProposal = Proposal::with('log')->orderBy('created_at', "DESC")->limit(5)->get();
+    
+    
+            // $arr_chart = [];
+            // for ($i = 1; $i <= 12; $i++) {
+            //     $arr_chart[] = Proposal::whereMonth('created_at', $i)->whereYear('created_at', Carbon::now()->year)->count();
+            // }
+    
+            $januari = Proposal::whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->count();
+            $februari = Proposal::whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->count();
+            $maret = Proposal::whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->count();
+            $april = Proposal::whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->count();
+            $mei = Proposal::whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->count();
+            $juni = Proposal::whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->count();
+            $juli = Proposal::whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->count();
+            $agustus = Proposal::whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->count();
+            $september = Proposal::whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->count();
+            $oktober = Proposal::whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->count();
+            $november = Proposal::whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->count();
+            $desember = Proposal::whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->count();
+    
+            $bansos = Anggaran::where('jenis_anggaran', 'Bantuan Sosial')->where('tahun_anggaran', Carbon::now()->format('Y'))->sum('nominal_anggaran');
+            $hibah = Anggaran::where('jenis_anggaran', 'Hibah')->where('tahun_anggaran', Carbon::now()->format('Y'))->sum('nominal_anggaran');
+    
+            $realisasibansos = Proposal::where('jenis_permohonan', 'Bantuan Sosial')->where('status', 'selesai')->sum('nominal_disetujui_walikota');
+            $realisasihibah = Proposal::where('jenis_permohonan', 'Hibah')->where('status', 'selesai')->sum('nominal_disetujui_walikota');
+    
+            // dd($arr_chart);
+            return view('dashboard', [
+                'setuju' => $setuju,
+                'proses' => $proses,
+                'all' => $all,
+                'total' => $total,
+                'pengguna' => $user,
+                'ditolak' => $ditolak,
+                'listproposal' => $listProposal,
+                'januari' => $januari,
+                'februari' => $februari,
+                'maret' => $maret,
+                'april' => $april,
+                'mei' => $mei,
+                'juni' => $juni,
+                'juli' => $juli,
+                'agustus' => $agustus,
+                'september' => $september,
+                'oktober' => $oktober,
+                'november' => $november,
+                'desember' => $desember,
+                'bansos' => intval($bansos),
+                'hibah' => intval($hibah),
+                'realisasibansos' => intval($realisasibansos),
+                'realisasihibah' => intval($realisasihibah),
             ]);
         }
 
-        $proses = Proposal::whereIn('is_status', ['2'])->count();
-        $setuju = Proposal::whereIn('is_status', ['1'])->where('status', 'Selesai')->count();
-        $all = Proposal::count();
-        $ditolak = Proposal::where('is_status', '0')->count();
-        $listProposal = Proposal::with('log')->orderBy('created_at', "DESC")->limit(5)->get();
-
-
-        // $arr_chart = [];
-        // for ($i = 1; $i <= 12; $i++) {
-        //     $arr_chart[] = Proposal::whereMonth('created_at', $i)->whereYear('created_at', Carbon::now()->year)->count();
-        // }
-
-        $januari = Proposal::whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->count();
-        $februari = Proposal::whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->count();
-        $maret = Proposal::whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->count();
-        $april = Proposal::whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->count();
-        $mei = Proposal::whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->count();
-        $juni = Proposal::whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->count();
-        $juli = Proposal::whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->count();
-        $agustus = Proposal::whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->count();
-        $september = Proposal::whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->count();
-        $oktober = Proposal::whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->count();
-        $november = Proposal::whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->count();
-        $desember = Proposal::whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->count();
-
-        $bansos = Anggaran::where('jenis_anggaran', 'Bantuan Sosial')->where('tahun_anggaran', Carbon::now()->format('Y'))->sum('nominal_anggaran');
-        $hibah = Anggaran::where('jenis_anggaran', 'Hibah')->where('tahun_anggaran', Carbon::now()->format('Y'))->sum('nominal_anggaran');
-
-        $realisasibansos = Proposal::where('jenis_permohonan', 'Bantuan Sosial')->where('status', 'selesai')->sum('nominal_disetujui_walikota');
-        $realisasihibah = Proposal::where('jenis_permohonan', 'Hibah')->where('status', 'selesai')->sum('nominal_disetujui_walikota');
-
-        // dd($arr_chart);
-        return view('dashboard', [
-            'setuju' => $setuju,
-            'proses' => $proses,
-            'all' => $all,
-            'total' => $total,
-            'pengguna' => $user,
-            'ditolak' => $ditolak,
-            'listproposal' => $listProposal,
-            'januari' => $januari,
-            'februari' => $februari,
-            'maret' => $maret,
-            'april' => $april,
-            'mei' => $mei,
-            'juni' => $juni,
-            'juli' => $juli,
-            'agustus' => $agustus,
-            'september' => $september,
-            'oktober' => $oktober,
-            'november' => $november,
-            'desember' => $desember,
-            'bansos' => intval($bansos),
-            'hibah' => intval($hibah),
-            'realisasibansos' => intval($realisasibansos),
-            'realisasihibah' => intval($realisasihibah),
-        ]);
+        
     }
     public function indexLanding()
     {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,15 +9,16 @@
                <div class="card">
                   <div class="card-header d-flex justify-content-between align-items-center">
                      <h5 class="card-title mb-0">Grafik Chart Pengajuan Tahun Ini</h5>
-                     <div class="dropdown">
-                        <button class="btn p-0" type="button" id="analyticsOptions" data-bs-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                        <i class="bx bx-dots-vertical-rounded"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="analyticsOptions">
-                           <a class="dropdown-item" href="javascript:void(0);">Select All</a>
-                           <a class="dropdown-item" href="javascript:void(0);">Refresh</a>
-                           <a class="dropdown-item" href="javascript:void(0);">Share</a>
+                     <div class="dropdown d-inline-flex">
+                        <form action="">
+                           <select name="periode" class="form-control" id="">
+                              <option value="" selected disabled> == Pilih Tahun == </option>
+                              @for ($i = 2020; $i < 2030; $i++)
+                                  <option value="{{ $i }}">{{ $i }}</option>
+                              @endfor
+                           </select>
+                           <button type="submit" class="form-control btn btn-info">Lihat</button>
+                        </form>
                         </div>
                      </div>
                   </div>
